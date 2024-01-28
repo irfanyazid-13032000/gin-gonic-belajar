eef7a65c4f03ed75cf0e20809433f8dfce2bfd30
irfanyazid-13032000 <irfanyazid18@gmail.com>
2024-01-28 16:34:59 +0700

    logging and basic auth

--- server.go
+++ server.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"gin-mnc/controller"
+	"gin-mnc/middleware"
 	"gin-mnc/service"
-
+	"io"
+	"os"
 	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
 )
 
 var(
@@ -12,8 +15,18 @@ var(
 	videoController controller.VideoController = controller.New(videoService)
 )
 
+func setupLogOutput(){
+	f,_ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f,os.Stdout)
+}
+
 func main() {
-	server := gin.Default()
+
+	setupLogOutput()
+
+	server := gin.New()
+
+	server.Use(gin.Recovery(),middleware.Logger(),middleware.BasicAuth(),gindump.Dump())
 
 	server.GET("/videos",func(ctx *gin.Context){
 		ctx.JSON(200,videoController.FindAll())
